6574393b80b317b1d77294107f23eb7c6592b548
Tapialj <ljptapia@gmail.com>
2021-09-15 21:12:47 -0500

    Finished

--- src/Assignment2_3.java
+++ src/Assignment2_3.java
@@ -1,14 +1,28 @@
 public class Assignment2_3 
 {
-    
+
+    private static final double convRatio = 2.2;
+
     public static void main(String[] args) throws Exception 
     {
+
         double[] kilo = {1, 3, 5, 7, 9, 11, 13, 15};
-        double[] pound = new double[kilo.length];
+        //double[] pound = new double[kilo.length];
         
 
         System.out.println("This program will display the conversion of kilograms to pounds on odd numbers up to 15.");
 
+        System.out.println("\t\tkilograms\tpounds");
+        System.out.println("\t\t---------\t------");
+
+        for(int i = 0; i < kilo.length; i++)
+        {
+            System.out.println("\t\t" + String.format("%.0f",kilo[i]) + "\t\t" + String.format("%.02f", (kilo[i] * convRatio)));
+        }
+
+        System.out.println("\n\nGoodbye...");
+
+        System.exit(0);
 
     }
 
